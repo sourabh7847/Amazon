dfc2efafdc0b593fb49849727a6085261c8f42f3
sourabh7847 <flank.valish@gmail.com>
2021-11-01 16:38:42 +0530

    landing page + basket count

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAD_AghUmh_vvf1eMAhMxbXmVMvveGtAvc",
+  authDomain: "clone-47445.firebaseapp.com",
+  projectId: "clone-47445",
+  storageBucket: "clone-47445.appspot.com",
+  messagingSenderId: "846253511277",
+  appId: "1:846253511277:web:0157d95663af8849dba564",
+  measurementId: "G-CLSMPV0Q74",
+};
